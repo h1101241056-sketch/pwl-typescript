b45f6981664a5460d7ee4ab9d06e7a6b1e779acb
Atikoh. a <h1101241056@student.untan.ac.id>
2026-02-25 04:46:24 +0000

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 function hello(nama: string, umur: number): string{
     return `Selamat Datang `+nama+` di pwl, umur `+nama+` adalah ${umur} tahun`;
 }
-const mahasiswa = "Andi";
-const umur:number = 19;
+const mahasiswa = "Atikoh";
+const umur:number = 20;
 
 console.log(hello(mahasiswa,umur));
