6a07b30b9c0c52cd1c6c82f2471b88cd71385deb
Atikoh. a <h1101241056@student.untan.ac.id>
2026-02-25 04:43:02 +0000

    update-index

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(nama) {
+    return `Selamat Datang ` + nama + ` di pwl`;
+}
+console.log(hello('tiko'));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+function hello(nama: string, umur: number): string{
+    return `Selamat Datang `+nama+` di pwl, umur `+nama+` adalah ${umur} tahun`;
+}
+const mahasiswa = "Andi";
+const umur:number = 19;
+
+console.log(hello(mahasiswa,umur));
